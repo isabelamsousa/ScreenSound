70fddfab6ba1f6902df3264a0add3939035bac67
isamaria <isamaria@MICRO02>
2025-10-02 12:14:16 -0300

    Atualizando projeto com novas funcionalidades e migrations

---
+++ ScreenSound/Migrations/20251002133314_PopularTabelaMusica.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelaMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "lomf", 2025 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Guilt as sin", 2025 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Peter", 2025 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "The tortured poets department", 2025 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20251002142826_PopularColunaArtistaID.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularColunaArtistaID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id = 4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistasId = NULL WHERE Id IN (1,2,3,4)");
+        }
+    }
+}
